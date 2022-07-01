c638d1cc2b71d876f25c21c01357c374f501b487
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-06-30 21:58:16 -0300

    feat: Controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { CustomersController } from './customers/customers.controller';
+import { UsersController } from './controllers/users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController, CustomersController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
